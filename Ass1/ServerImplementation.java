691d0f304391eecffee257b1712afbc75c747f3c
Anurag Patil <113289237+anuragpatil11@users.noreply.github.com>
2025-05-10 03:01:30 +0530

    Create ServerImplementation.java

---
+++ Ass1/ServerImplementation.java
@@ -0,0 +1,26 @@
+import java.rmi.*;
+import java.rmi.server.*;
+
+public class ServerImplementation extends UnicastRemoteObject implements ServerInterface {
+	public ServerImplementation() throws RemoteException {}
+
+	public double Addition(double num1, double num2) throws RemoteException {
+		return num1 + num2;
+	}
+
+	public double Subtraction(double num1, double num2) throws RemoteException {
+		return num1 - num2;
+	}
+
+	public double Division(double num1, double num2) throws RemoteException {
+		if (num2 == 0) {
+			System.out.println("Cannot divide with zero!");
+			return -1;
+		}
+		return num1 / num2;
+	}
+
+	public double Multiplication(double num1, double num2) throws RemoteException {
+		return num1 * num2;
+	}
+}
