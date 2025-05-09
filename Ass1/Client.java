1ab103291e819d9e005dd5e92b9f33e359ebb81b
Anurag Patil <113289237+anuragpatil11@users.noreply.github.com>
2025-05-10 02:59:55 +0530

    Create Client.java

---
+++ Ass1/Client.java
@@ -0,0 +1,27 @@
+import java.rmi.*;
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String[] args) {
+		try {
+			String serverUrl = "rmi://localhost/Server";
+			ServerInterface serverInterface = (ServerInterface) Naming.lookup(serverUrl);
+			Scanner sc = new Scanner(System.in);
+			double num1;
+			double num2;
+			System.out.print("Enter number 1:- ");
+			num1 = sc.nextDouble();
+			System.out.print("Enter number 2:- ");
+			num2 = sc.nextDouble();
+			System.out.println("Number 1 is:- " + num1);
+			System.out.println("Number 2 is:- " + num2);
+			System.out.println("-----------Results------------");
+			System.out.println(num1 + " + " + num2 + " = " + serverInterface.Addition(num1, num2));
+			System.out.println(num1 + " - " + num2 + " = " + serverInterface.Subtraction(num1, num2));
+			System.out.println(num1 + " / " + num2 + " = " + serverInterface.Division(num1, num2));
+			System.out.println(num1 + " x " + num2 + " = " + serverInterface.Multiplication(num1, num2));
+		} catch (Exception e) {
+			System.out.println("Something went wrong! " + e.getMessage());
+		}
+	}
+}
