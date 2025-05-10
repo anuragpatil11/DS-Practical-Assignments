c50ec40d826e90cce1ae773bd51699fd32af59ce
Anurag Patil <113289237+anuragpatil11@users.noreply.github.com>
2025-05-10 09:33:21 +0530

    Update ServerInterface.java

--- Ass1/ServerInterface.java
+++ Ass1/ServerInterface.java
@@ -1,5 +1,6 @@
 import java.rmi.*;
 
+// Remote interface that declares methods which can be called remotely by clients
 interface ServerInterface extends Remote {
 	public double Addition(double num1, double num2) throws RemoteException;
 	public double Subtraction(double num1, double num2) throws RemoteException;
