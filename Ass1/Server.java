141f5c4527ee0b064fc1dbf75a96bd3fa08cb4d5
Anurag Patil <113289237+anuragpatil11@users.noreply.github.com>
2025-05-10 09:31:52 +0530

    Update Server.java

--- Ass1/Server.java
+++ Ass1/Server.java
@@ -3,8 +3,12 @@ import java.rmi.*;
 public class Server {
 	public static void main(String[] args) {
 		try {
+			// Create an instance of the class that implements the remote interface
 			ServerImplementation serverImplementation = new ServerImplementation();
+
+			// Bind the remote object to the RMI registry using the name "Server"
 			Naming.rebind("Server", serverImplementation);
+
 			System.out.println("Server is active...");
 		} catch (Exception e) {
 			System.out.println("Something went wrong! " + e.getMessage());
